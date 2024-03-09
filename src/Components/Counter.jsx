d2016b003ce8dd6c47076951e40ae5287163cd50
Behnam Hamidi <eslp91@hotmail.com>
2024-03-09 14:28:47 +0330

    counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,29 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+  inputNumber,
+} from "../features/counterSlice/counterSlice";
 
 const Counter = () => {
+  const [number, setNumber] = useState("");
+
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (number) {
+      dispatch(inputNumber(number));
+      setNumber("");
+    }
+  };
+
   return (
     <div className="text-white">
-      <div className="text-6xl">0</div>
+      <div className="text-6xl">{counter}</div>
       <div className="btns flex gap-3 mt-10">
-        <button className="bg-green-600 px-4 py-2 rounded-md font-semibold">
+        <button
+          className="bg-green-600 px-4 py-2 rounded-md font-semibold"
+          onClick={() => dispatch(increment())}
+        >
           increment +
         </button>
-        <button className="bg-red-600 px-4 py-2 rounded-md font-semibold">
+        <button
+          className="bg-red-600 px-4 py-2 rounded-md font-semibold"
+          onClick={() => dispatch(decrement())}
+        >
           decrement -
         </button>
-        <button className="bg-gray-500 px-4 py-2 rounded-md font-semibold">
+        <button
+          className="bg-gray-500 px-4 py-2 rounded-md font-semibold"
+          onClick={() => dispatch(incrementByAmount(3))}
+        >
           increment by 3
         </button>
-        <button className="bg-gray-600 px-4 py-2 rounded-md font-semibold">
+        <button
+          className="bg-gray-600 px-4 py-2 rounded-md font-semibold"
+          onClick={() => dispatch(decrementByAmount(3))}
+        >
           decrement by 3
         </button>
-        <button className="bg-blue-600 px-4 py-2 rounded-md font-semibold">
+        <button
+          className="bg-blue-600 px-4 py-2 rounded-md font-semibold"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
           increment by 5
         </button>
-        <button className="bg-blue-800 px-4 py-2 rounded-md font-semibold">
+        <button
+          className="bg-blue-800 px-4 py-2 rounded-md font-semibold"
+          onClick={() => dispatch(decrementByAmount(5))}
+        >
           decrement by 5
         </button>
       </div>
+      <form className="mt-10" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="px-2 py-1 rounded-md text-[12px] w-[300px] text-black outline-none"
+          placeholder="Enter a number ... "
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button className="bg-purple-700 px-2 py-1 rounded-md ml-2 text-[12px] cursor-pointer">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- src/features/counterSlice/counterSlice.js
+++ src/features/counterSlice/counterSlice.js
@@ -11,8 +11,36 @@ const counterSlice = createSlice({
     increment: (state) => {
       state.value += 1;
     },
+    decrement: (state) => {
+      if (state.value === 0) {
+        return;
+      } else {
+        state.value -= 1;
+      }
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      let result = state.value - action.payload;
+
+      if (result <= 0) {
+        return initialState;
+      } else {
+        state.value -= action.payload;
+      }
+    },
+    inputNumber: (state, action) => {
+      state.value = action.payload;
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+  inputNumber,
+} = counterSlice.actions;
 export default counterSlice.reducer;
